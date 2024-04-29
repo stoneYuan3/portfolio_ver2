2ed7b9638e4907d0935793fa1652061818efe325
stoneYuan3 <jackyuan110@gmail.com>
2024-04-29 15:46:22 -0700

    sela proj update

--- src/pages/work_articles/programming/selaProject.js
+++ src/pages/work_articles/programming/selaProject.js
@@ -44,6 +44,7 @@ export const SelaProject=(props)=>{
 
                         {/* new work intro */}
                         <WorkIntro 
+                            banner={"/img/gallery/sela/banner.png"}
                             workType={"Front-end development"}
                             workSkill={"HTML, CSS, Typescript, React.js, Git"}
                             workContext={"Volunteer project, Team "}
@@ -85,79 +86,79 @@ export const SelaProject=(props)=>{
                                 </ul>
                             </p>
                         </div>
+                        
+                        {/* <div>
+                            <div className="sectionList demoHead">
+                                <h2>Demo:</h2>
+                            </div>
+                            <div className="sectionList demoHead">
+                                <img src="/img/gallery/sela/interface-current.png"/>
+                                <p className="demoComments">The current interface</p>
+                            </div>
+                            <div className="sectionList">
+                                <img src="/img/gallery/sela/demo-changeColour.gif"/>
+                                <p className="demoComments">Newest feature: select and change colour</p>
+                            </div>
 
-                        <div className="sectionList demoHead">
-                            <h2>Demo:</h2>
-                        </div>
-                        <div className="sectionList demoHead">
-                            <img src="/img/gallery/sela/interface-current.png"/>
-                            <p className="demoComments">The current interface</p>
-                        </div>
-                        <div className="sectionList">
-                            <img src="/img/gallery/sela/demo-changeColour.gif"/>
-                            <p className="demoComments">Newest feature: select and change colour</p>
-                        </div>
-
-                        <div className="sectionList">
-                            <h2>Process/Challenge:</h2>
-                            <p>
-                                This project is one of the most challenging projects I encountered so far. Our initial inspiration comes from an existing Bible reading tool called <a href="https://www.stepbible.org/">STEP Bible</a>, which is a fairly complex application. Some of our basic front-end features inspired by the STEP Bible will allow users to select each individual bible words, highlight those words in different colour and so on. Those functionalities are quite new to me given my previous experience with React.js.
-                            </p>
-                            <p>
-                                To tackle those challenges, I used a combination of generative AI tools and the official document of React to quickly equip myself the necessary skills. The general procedure I took is listed below:
-                            </p>
-                            <p>
-                                    <ol>
-                                        {/* <li><strong>1.	Typeface</strong> <br/> */}
-                                        <li>
-                                            <strong>1</strong> Build the interface of the feature based on what I already know
-                                        </li>
-                                        <li>
-                                            <strong>2</strong> Ask ChatGPT how to realize the feature based on my current interface structure
-                                        </li>
-                                        <li>
-                                            <strong>3</strong> Proceed to React.js's official documentation to learn more about the tools involved in ChatGPT's responses, potentially go back to step 1 to rebuild the interface structure
-                                        </li>
-                                        <li>
-                                            <strong>4</strong> Test and experiment with the result. If it works, proceed to the next feature. If not, go back to step 2.
-                                        </li>
-                                    </ol>
+                            <div className="sectionList">
+                                <h2>Process/Challenge:</h2>
+                                <p>
+                                    This project is one of the most challenging projects I encountered so far. Our initial inspiration comes from an existing Bible reading tool called <a href="https://www.stepbible.org/">STEP Bible</a>, which is a fairly complex application. Some of our basic front-end features inspired by the STEP Bible will allow users to select each individual bible words, highlight those words in different colour and so on. Those functionalities are quite new to me given my previous experience with React.js.
                                 </p>
-                        </div>
+                                <p>
+                                    To tackle those challenges, I used a combination of generative AI tools and the official document of React to quickly equip myself the necessary skills. The general procedure I took is listed below:
+                                </p>
+                                <p>
+                                        <ol>
+                                            <li>
+                                                <strong>1</strong> Build the interface of the feature based on what I already know
+                                            </li>
+                                            <li>
+                                                <strong>2</strong> Ask ChatGPT how to realize the feature based on my current interface structure
+                                            </li>
+                                            <li>
+                                                <strong>3</strong> Proceed to React.js's official documentation to learn more about the tools involved in ChatGPT's responses, potentially go back to step 1 to rebuild the interface structure
+                                            </li>
+                                            <li>
+                                                <strong>4</strong> Test and experiment with the result. If it works, proceed to the next feature. If not, go back to step 2.
+                                            </li>
+                                        </ol>
+                                    </p>
+                            </div>
 
-                        <div className="sectionList">
-                            <h3>Case Study: Adjusting Font Size Feature</h3>
-                            <p>
-                                One of the features that my team would like to add is that they want to allow users to change the font size of Bible poetry word boxes.
-                            </p>
-                            <p>This is not the most complex feature I implemented so far, but it best represents my problem-solving process. The knowledge I gained from this feature also helped me to develop a deeper understanding towards React and helped me to implement other features more smoothly.</p>
-                            <img src="/img/gallery/sela/wordbox.png"/>
-                            <p className="demoComments">Each "word box" is wrapped by a border. Each word box represents one word in Hebrew</p>
-                        </div>
-                        <div className="sectionList">
-                            <p>
-                                At the time I started working on this feature, I already completed the necessary interface components for this feature. The structure of my interface components is shown below. Based on my previous React.js experience, I made an educated guess where I put the body content component (the poem words) under the navigation component as a child component.
-                            </p>
-                            <img src="/img/gallery/sela/structure1.png"/>
-                        </div>
-                        <div className="sectionList">
-                            <p>
-                                To get started, I asked ChatGPT about how to implement this function based on my codes. I find that this feature can be easily achieved using React state management. I have used React state management briefly when developing this portfolio, but I haven't realized that it can also be used in this way.
-                            </p>
-                            <img src="/img/gallery/sela/gpt1.png"/>
-                            <p className="demoComments">A screenshot showing how I started the conversation with ChatGPT</p>
-                        </div>
-                        <div className="sectionList">
-                            <p>
-                                After double-checking with React.js's official document, I planned out how to execute this feature mainly on paper. I wrote down my current interface structure and planned out where I should put the font size-related function based on what I learned from ChatGPT and React's official documentation. I then implemented the feature successfully based on the note.
-                            </p>
-                            <img src="/img/gallery/sela/note1.png"/>
-                            <p className="demoComments">My notes for this feature. It also has plans for other features such as changing the word box colour.</p>
-                            <img src="/img/gallery/sela/demo-fontsize.gif"/>
-                            <p className="demoComments">Result</p>
-                        </div>
-
-
+                            <div className="sectionList">
+                                <h3>Case Study: Adjusting Font Size Feature</h3>
+                                <p>
+                                    One of the features that my team would like to add is that they want to allow users to change the font size of Bible poetry word boxes.
+                                </p>
+                                <p>This is not the most complex feature I implemented so far, but it best represents my problem-solving process. The knowledge I gained from this feature also helped me to develop a deeper understanding towards React and helped me to implement other features more smoothly.</p>
+                                <img src="/img/gallery/sela/wordbox.png"/>
+                                <p className="demoComments">Each "word box" is wrapped by a border. Each word box represents one word in Hebrew</p>
+                            </div>
+                            <div className="sectionList">
+                                <p>
+                                    At the time I started working on this feature, I already completed the necessary interface components for this feature. The structure of my interface components is shown below. Based on my previous React.js experience, I made an educated guess where I put the body content component (the poem words) under the navigation component as a child component.
+                                </p>
+                                <img src="/img/gallery/sela/structure1.png"/>
+                            </div>
+                            <div className="sectionList">
+                                <p>
+                                    To get started, I asked ChatGPT about how to implement this function based on my codes. I find that this feature can be easily achieved using React state management. I have used React state management briefly when developing this portfolio, but I haven't realized that it can also be used in this way.
+                                </p>
+                                <img src="/img/gallery/sela/gpt1.png"/>
+                                <p className="demoComments">A screenshot showing how I started the conversation with ChatGPT</p>
+                            </div>
+                            <div className="sectionList">
+                                <p>
+                                    After double-checking with React.js's official document, I planned out how to execute this feature mainly on paper. I wrote down my current interface structure and planned out where I should put the font size-related function based on what I learned from ChatGPT and React's official documentation. I then implemented the feature successfully based on the note.
+                                </p>
+                                <img src="/img/gallery/sela/note1.png"/>
+                                <p className="demoComments">My notes for this feature. It also has plans for other features such as changing the word box colour.</p>
+                                <img src="/img/gallery/sela/demo-fontsize.gif"/>
+                                <p className="demoComments">Result</p>
+                            </div>
+                        </div> */}
+                        
                     </div>
 
 
